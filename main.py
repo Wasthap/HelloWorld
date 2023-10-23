84b8cf8ed4f0a85a634c6ff0eaf29bcff4bf96bd
Wasthap <148814319+Wasthap@users.noreply.github.com>
2023-10-23 21:56:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -116,3 +116,9 @@ df2 = pd.DataFrame(
 c = alt.Chart(df2).mark_circle().encode(
      x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
+
+chart_data = pd.DataFrame(
+     np.random.randn(20, 3),
+     columns=['a', 'b', 'c'])
+
+st.line_chart(chart_data)
