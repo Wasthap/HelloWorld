f9a5bad3ec73b7bab312aa58112ff3a7f5612bb9
Wasthap <148814319+Wasthap@users.noreply.github.com>
2023-10-23 22:01:26 +0200

    Update main.py

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ st.write('Ci-dessous se trouve un DataFrame:', df, 'Ci-dessus se trouve un DataF
 
 df2 = pd.DataFrame(
      np.random.randn(200, 3),
-     columns=['amp', 'b', 'c'])
+     columns=['a', 'b', 'c'])
 c = alt.Chart(df2).mark_circle().encode(
-     x='amp', y='b', size='c', color='c', tooltip=['amp', 'b', 'c'])
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
 st.write(c)
 
 chart_data = pd.DataFrame(
